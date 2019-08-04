9ee8e6cb0bd1bba3e2a84536ca44b1628f72aa51
MrCekolek <MrCekolek12345>
2019-08-04 10:47:01 +0200

    basic configuration - version 3 - jwt - version 1

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { RequestResetComponent } from './components/auth/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/auth/password/response-reset/response-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule,
