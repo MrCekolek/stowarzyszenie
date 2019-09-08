7d07f527b214ef5cfefff69e3256f278e69ed585
Małgorzata <malgorzata@pietrewicz.com.pl>
2019-09-08 10:24:31 +0200

    Navbar górny z działającymi routes

--- front/src/app/modules/navigation/navigation.module.ts
+++ front/src/app/modules/navigation/navigation.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    AppRoutingModule
   ],
   exports: [NavbarComponent],
   providers: []
