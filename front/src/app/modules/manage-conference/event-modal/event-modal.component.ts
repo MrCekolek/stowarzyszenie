79c1f602e1798ed1e0047388ebd3e3ecbadb167a
Małgorzata Pietrewicz <malgorzata@pietrewicz.com.pl>
2020-04-11 09:44:50 +0200

    pare zmian do merge'a

--- front/src/app/modules/manage-conference/event-modal/event-modal.component.ts
+++ front/src/app/modules/manage-conference/event-modal/event-modal.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ApiService } from 'src/app/core/http/api.service';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ManageConferenceApiService } from 'src/app/core/http/manage-conference-api.service';
 import { Event } from 'src/app/shared/models/event.model';
+import { LanguageService } from 'src/app/shared/services/user/language.service';
 
 @Component({
   selector: 'app-event-modal',
@@ -12,6 +13,7 @@ import { Event } from 'src/app/shared/models/event.model';
 export class EventModalComponent implements OnInit {
 
   private translations = [];
+  private descTranslations = [];
   private event: Event = {
     name_pl: '',
     name_en: '',
@@ -25,12 +27,35 @@ export class EventModalComponent implements OnInit {
   };
   private addLoading;
   private loading;
+  private descLoading;
+  private modal_type: string;
+  private conference_id;
 
   constructor(
-    private apiService: ApiService,
     private dialogRef: MatDialogRef<EventModalComponent>,
-    private conferenceApi: ManageConferenceApiService
-  ) { }
+    @Inject(MAT_DIALOG_DATA) data,
+    private languageService: LanguageService,
+    private conferenceApi: ManageConferenceApiService,
+    private apiService: ApiService
+  ) {
+    if (data.event) {
+      this.event = data.event;
+
+      this.translations[0] = data.event.name_pl;
+      this.translations[1] = data.event.name_en;
+      this.translations[2] = data.event.name_ru;
+
+      this.descTranslations[0] = data.event.description_pl;
+      this.descTranslations[1] = data.event.description_en;
+      this.descTranslations[2] = data.event.description_ru;
+    }
+
+    if (data.conference_id) {
+      this.conference_id = data.conference_id;
+    }
+
+    this.modal_type = data.modal_type;
+   }
 
   ngOnInit() {
     this.conferenceApi.getConference().subscribe(res => {
@@ -54,6 +79,25 @@ export class EventModalComponent implements OnInit {
       this.event.name_ru = response.translation.name_ru;
 
       this.addLoading = false;
+    });
+  }
+
+  getDescTranslations(input) {
+    const obj = {
+      name: input
+    };
+
+    this.descLoading = true;
+    this.apiService.post('translation/get', obj).subscribe(response => {
+      this.descTranslations[0] = response.translation.name_pl;
+      this.descTranslations[1] = response.translation.name_en;
+      this.descTranslations[2] = response.translation.name_ru;
+
+      this.event.description_pl = response.translation.name_pl;
+      this.event.description_en = response.translation.name_en;
+      this.event.description_ru = response.translation.name_ru;
+
+      this.descLoading = false;
     });
   }
 
